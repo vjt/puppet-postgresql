7effd9e65c36788ec4f2579c02776d26a290e993
Marcello Barnaba <vjt@openssl.it>
2012-07-28 15:08:39 +0200

    Implement database removal

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,20 +1,37 @@
 define postgresql::db (
     $password,
-    $owner = $name,
+    $ensure   = present,
+    $owner    = $name,
     $encoding = 'UTF8',
-    $locale = 'en_GB.UTF-8',
+    $locale   = 'en_US.UTF8',
 ) {
 
-    pg_user {$owner:
-        ensure      => present,
-        password    => $password,
+  case $ensure {
+    present : {
+      pg_user {$owner:
+        ensure   => present,
+        password => $password,
+        require  => Service['postgresql'],
+      }
+
+      pg_database {$name:
+        ensure    => present,
+        owner     => $owner,
+        require   => Pg_user[$owner],
+        encoding  => $encoding,
+        locale    => $locale,
+      }
     }
 
-    pg_database {$name:
-        ensure      => present,
-        owner       => $owner,
-        require     => Pg_user[$owner],
-        encoding    => $encoding,
-        locale      => $locale,
+    absent : {
+      pg_user {$owner:
+        ensure  => absent,
+        require => Pg_database[$name],
+      }
+      pg_database {$name:
+        ensure  => absent,
+        require => Service['postgresql'],
+      }
     }
+  }
 }
