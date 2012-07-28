8209320fef78c9d8b062d77a2c9248f4ccca39c8
Marcello Barnaba <vjt@openssl.it>
2012-07-28 15:08:23 +0200

    Rewrite operating systems support

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,33 @@
 class postgresql::params {
-  case $::operatingsystem {
-    /(Ubuntu|Debian)/: {
-      $version = '9.1'
-      $client_package = 'postgresql-client'
-      $server_package = 'postgresql'
-      $listen_address = 'localhost'
-      $port = 5432
+  $version        = '9.1'
+  $listen_address = 'localhost'
+  $port           = '5432'
+  $user           = 'postgres'
+  $group          = 'postgres'
+
+  case $::osfamily {
+    debian: {
+      $client_package = "postgresql-client-${version}"
+      $server_package = "postgresql-${version}"
+
+      $datadir = "/var/lib/postgresql/${version}/main"
+      $confdir = "/etc/postgresql/${version}/main"
+      $sockdir = "/var/run/postgresql"
+      $pidfile = "/var/run/postgresql/${version}-main.pid"
+      $initdb  = "/usr/lib/postgresql/${version}/bin/initdb"
     }
+
+    suse: {
+      $client_package = 'postgresql'
+      $server_package = 'postgresql-server'
+
+      $datadir = '/var/lib/pgsql/data'
+      $confdir = $datadir
+      $sockdir = "/tmp"
+      $pidfile = false
+      $initdb  = "/usr/bin/initdb"
+    }
+
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,35 +1,66 @@
 class postgresql::server (
-  $server_package = $postgresql::params::server_package,
   $version = $postgresql::params::version,
-  $listen = $postgresql::params::listen_address,
-  $port = $postgresql::params::port
-
+  $listen  = $postgresql::params::listen_address,
+  $port    = $postgresql::params::port,
+  $acls    = [],
 ) inherits postgresql::params {
 
-  package { "postgresql-server-$version":
-    name    => sprintf("%s-%s", $server_package, $version),
-    ensure  => present,
+  package { "postgresql-server-${version}":
+    name   => $server_package,
+    ensure => present,
+    before => Datadir["postgresql-$version"],
   }
 
-  service { "postgresql-system-$version":
-    name        => 'postgresql',
-    enable      => true,
-    ensure      => running,
-    hasstatus   => false,
-    hasrestart  => true,
-    provider    => 'debian',
-    subscribe   => Package["postgresql-server-$version"],
+  datadir { "postgresql-${version}":
+    ensure => present
   }
 
-  file { "postgresql-server-config-$version":
-    name    => "/etc/postgresql/$version/main/postgresql.conf",
-    ensure  => present,
-    content => template('postgresql/postgresql.conf'),
-    owner   => 'postgres',
-    group   => 'postgres',
-    mode    => '0644',
-    require => Package["postgresql-server-$version"],
-    notify  => Service["postgresql-system-$version"],
+  service { "postgresql-$version":
+    name      => 'postgresql',
+    ensure    => running,
+    require   => Config["postgresql-server-config-$version"],
+    subscribe => Package["postgresql-server-$version"],
+  }
+
+  config {
+    "postgresql-server-config-$version":
+      name   => 'postgresql.conf',
+      ensure => present;
+
+    "postgresql-server-access-$version":
+      name    => 'pg_hba.conf',
+      ensure  => present,
+  }
+
+  define config($ensure, $name) {
+    file { $title:
+      name    => "${confdir}/${name}",
+      content => template("postgresql/${name}.erb"),
+      ensure  => $ensure,
+      owner   => $user,
+      group   => $group,
+      mode    => '0600',
+      require => Datadir["postgresql-$version"],
+      notify  => Service["postgresql-$version"],
+    }
+  }
+
+  define datadir($ensure) {
+    file { $datadir:
+      ensure  => directory,
+      owner   => $user,
+      group   => $group,
+      mode    => '0700',
+      before  => Exec["postgresql-initdb-${version}"],
+    }
+
+    exec { "postgresql-initdb-${version}":
+      user      => $user,
+      group     => $group,
+      creates   => "${datadir}/PG_VERSION",
+      command   => "${initdb} --locale=en_US.UTF8 ${datadir}",
+      logoutput => true,
+    }
   }
 
 }
